305b77c5c8fc426e49d5b51078c07f045cf94576
caterina mecheri <cate3rina@hotmail.com>
2022-05-07 16:39:34 +0100

    migrated db after modifying schema with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_07_144625) do
+ActiveRecord::Schema.define(version: 2022_05_07_153702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_05_07_144625) do
     t.integer "berth"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
