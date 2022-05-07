76c4eb6548989aee919dc71162c1c2b5d5727818
caterina mecheri <cate3rina@hotmail.com>
2022-05-07 15:47:18 +0100

    created bookings model with references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_07_142124) do
+ActiveRecord::Schema.define(version: 2022_05_07_144625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 2022_05_07_142124) do
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.float "cost"
+    t.bigint "user_id", null: false
+    t.bigint "boat_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["boat_id"], name: "index_bookings_on_boat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,4 +52,6 @@ ActiveRecord::Schema.define(version: 2022_05_07_142124) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "boats"
+  add_foreign_key "bookings", "users"
 end
