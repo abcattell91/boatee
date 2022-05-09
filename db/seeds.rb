944013baaa5b86fab14994e6522c12957363723f
Mustafa Alomer <mustafa_05@msn.com>
2022-05-09 22:40:38 +0100

    added a basic seed for boat creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,8 @@
 require 'faker'
+
+puts "Cleaning up database..."
+Boat.destroy_all
+Booking.destroy_all
+puts "Database cleaned"
+
+Boat.create!(name: "Titanic", boat_type: "Ship", docked: "yes", location: "London", price: 100, berth: 200)
