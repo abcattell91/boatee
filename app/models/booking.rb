9e6bd8d6ac7a528e2867a61435c212845874f670
Mustafa Alomer <mustafa_05@msn.com>
2022-05-10 21:26:44 +0100

    changed the form settings via flatpickr

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,3 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :boat
 end
+
+def unavailable_dates
+  bookings.pluck(:start_on, :end_on).map do |range|
+    { from: range[0], to: range[1] }
+  end
+end
