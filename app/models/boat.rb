6a7a27ec1af22d5abe5a947bee01eb5e94bfa00f
Alex Cattell <abcattell91@gmail.com>
2022-05-10 20:26:35 +0100

    Put in boat category validations and form

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,6 @@
 class Boat < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
-  validates :name, :boat_type, :docked, :location, :price, :berth, presence: true
+  validates :name, :docked, :location, :price, :berth, presence: true
+  validates :boat_type, inclusion: { in: %w[Bracera Banana Boat Barge Dinghy Pedalo Punt Super Yacht Ship Catamaran Hovercraft] }
 end
