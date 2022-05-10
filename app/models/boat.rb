b88f9de2d5f3988c4bdb3579548de3890198849b
Mustafa Alomer <mustafa_05@msn.com>
2022-05-10 22:09:25 +0100

    refactored the boat model

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,8 +1,9 @@
 class Boat < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
+  BOAT_CATEGORIES = %w[Bracera Banana-Boat Barge Dinghy Pedalo Punt Yacht Ship Catamaran Hovercraft]
   validates :name, :location, :price, :image_url, presence: true
-  validates :boat_type, inclusion: { in: %w[Bracera Banana-Boat Barge Dinghy Pedalo Punt Yacht Ship Catamaran Hovercraft] }
+  validates :boat_type, inclusion: { in: BOAT_CATEGORIES }
   validates :image_url, presence: true, allow_blank: true
   validates :guests, inclusion: { in: 0..100 }, numericality: { only_integer: true }
   validates :docked, inclusion: [true, false]
