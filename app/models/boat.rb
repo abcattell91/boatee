82c7b7bd218ec30872a078137034e408d68fae9f
Alex Cattell <abcattell91@gmail.com>
2022-05-11 16:03:35 +0100

    added regex validation to boat image_url to make sure a url is only allowed to be input

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,7 +4,7 @@ class Boat < ApplicationRecord
   BOAT_CATEGORIES = %w[Bracera Banana-Boat Barge Dinghy Pedalo Punt Yacht Ship Catamaran Hovercraft]
   validates :name, :location, :price, :image_url, presence: true
   validates :boat_type, inclusion: { in: BOAT_CATEGORIES }
-  validates :image_url, presence: true, allow_blank: true
+  validates :image_url, presence: true, allow_blank: true, format: { with: /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-=\?]*)*\/?/ }
   validates :guests, inclusion: { in: 0..100 }, numericality: { only_integer: true }
   validates :docked, inclusion: [true, false]
 
