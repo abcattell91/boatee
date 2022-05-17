17555eb4bc10d5defd3fd565688d1a6017a0d9cc
Alex Cattell <abcattell91@gmail.com>
2022-05-17 22:02:00 +0100

    have working cards on home page with links to each boat. Need to style them into the correct style of card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @boats = Boat.limit(6).order('created_at DESC');
+    @bookings = Booking.limit(1).order('created_at DESC');
   end
 end
