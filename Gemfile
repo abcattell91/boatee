f8a88e4cbb8198df1cd50eb939d365b6abcfe1b6
Mustafa Alomer <mustafa_05@msn.com>
2022-05-19 21:32:45 +0100

    search bar

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
 gem 'simple_form', github: 'heartcombo/simple_form'
 
+# pg search gem
+gem 'pg_search', '~> 2.3.0'
+
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,13 @@ ActiveRecord::Schema.define(version: 2022_05_13_082051) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "picture_url"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "token"
+    t.datetime "token_expiry"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
